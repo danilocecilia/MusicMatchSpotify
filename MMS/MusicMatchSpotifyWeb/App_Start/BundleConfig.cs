8ec340d426e26d6e4766f3b5582b06e6f7b989d6
Danilo Cecilia <danilo.cecilia@gmail.com>
2015-09-04 11:21:28 -0300

    Adding the play button functionality and the possibility to search tracks on each playlist

--- MMS/MusicMatchSpotifyWeb/App_Start/BundleConfig.cs
+++ MMS/MusicMatchSpotifyWeb/App_Start/BundleConfig.cs
@@ -18,6 +18,9 @@ namespace MusicMatchSpotifyWeb
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/hideseek").Include(
+                        "~/Scripts/jquery.hideseek.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
